b4466390765dfb56515d494a527d6e808f5dda4a
Aiya <ichanel@mail.ru>
2020-04-13 17:55:10 +0600

    Исрпавление опечаток
    Signed-off-by: Aiya <ichanel@mail.ru>

--- script.js
+++ script.js
@@ -6,10 +6,20 @@ let phrases = [
     'расставить книги на полке по цвету',
     'читать про зарплаты в Сан-Франциско'
 ];
+
 function getRandomElement(arr) {
-    let randIndex = Math.floor(Math.random * phrases.length);
+    let randIndex = Math.floor(Math.random() * arr.length);
     return arr[randIndex];
 }
+
 let button = document.querySelector('.button');
+let phrase = document.querySelector('.phrase');
+let advice = document.querySelector('.advice');
+let image = document.querySelector('.image');
 
 phrase.textContent = getRandomElement(phrases);
+
+button.addEventListener('click', function(){
+        phrase.textContent = getRandomElement(phrases);
+    }
+);
